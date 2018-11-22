a53f14caeb1a4eddaadde7df197a28bc6f5a5c42
Jaydeep Solanki <jaydp17@gmail.com>
2018-11-22 16:52:50 +0530

    add EsBulkInsertResult type 🏷

--- typedefs.js
+++ typedefs.js
@@ -35,3 +35,20 @@
  * @prop {number} updatedAt
  * @prop {number} __v
  */
+
+/**
+ * @typedef {object} EsBulkInsertResult
+ * @prop {boolean} errors
+ * @prop {number} took
+ * @prop {object[]} items
+ * @prop {object} items.index
+ * @prop {string} items.index._id
+ * @prop {string} items.index._type
+ * @prop {number} items.index.status
+ * @prop {object} [items.index.error]
+ * @prop {string} items.index.error.index
+ * @prop {string} items.index.error.index_uuid
+ * @prop {string} items.index.error.reason
+ * @prop {string} items.index.error.shard
+ * @prop {string} items.index.error.type exception type "version_conflict_engine_exception"
+ */
