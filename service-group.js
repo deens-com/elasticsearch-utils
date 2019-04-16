477bd80e9c941aadd377f42e89dc982e2da275bb
Jaydeep Solanki <jaydp17@gmail.com>
2019-04-16 23:24:53 +0530

    Revert "add trip.pricing"
    This reverts commit 73b2f9ff80ea90e4fc027b3e2418b3f37cfeed85.

--- service-group.js
+++ service-group.js
@@ -12,14 +12,6 @@ function keepOnlyRequiredFields(trip) {
     cloneDate: parent.cloneDate.getTime(),
   }));
   const totalDays = getMaxValue(trip.services, serviceOrg => serviceOrg.day);
-  /* eslint-disable camelcase */
-  const p1_0_0 = findPricingElement(trip, 1, 0, 0);
-  const p1_1_0 = findPricingElement(trip, 1, 1, 0);
-  const p1_2_0 = findPricingElement(trip, 1, 2, 0);
-  const p2_0_0 = findPricingElement(trip, 2, 0, 0);
-  const p2_1_0 = findPricingElement(trip, 2, 1, 0);
-  const p2_2_0 = findPricingElement(trip, 2, 2, 0);
-  /* eslint-enable camelcase */
   return {
     _id: trip._id.toString(),
     status: trip.status,
@@ -43,7 +35,6 @@ function keepOnlyRequiredFields(trip) {
       trip.media[0].files &&
       trip.media[0].files.hero &&
       trip.media[0].files.hero.url,
-    pricing: { p1_0_0, p1_1_0, p1_2_0, p2_0_0, p2_1_0, p2_2_0 },
     createdAt: trip.createdAt.getTime(),
     updatedAt: trip.updatedAt.getTime(),
     __v: trip.__v,
@@ -75,25 +66,6 @@ function generateEsBody(indexableServices, { _index, _type = '_doc' }) {
     ];
   });
   return flatten(nestedEsBody);
-}
-
-/**
- * Finds the price & pricePerDay for given peopleCount from `trip.pricing`
- * @param {PartialServiceGroup} trip
- * @param {number} adultCount
- * @param {number} childrenCount
- * @param {number} infantCount
- */
-function findPricingElement(trip, adultCount, childrenCount, infantCount) {
-  const pricingElement = (trip.pricing || []).find(
-    p =>
-      p.adultCount === adultCount &&
-      p.childrenCount === childrenCount &&
-      p.infantCount === infantCount,
-  );
-  if (pricingElement)
-    return { price: pricingElement.price, pricePerDay: pricingElement.pricePerDay };
-  return { price: 0, pricePerDay: 0 };
 }
 
 module.exports = { keepOnlyRequiredFields, generateEsBody };
